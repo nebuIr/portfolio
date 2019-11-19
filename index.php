908ac394694134429b49b3142e1c95e21ac9d506
prime@nebulr.org <fabian.brendli@dotsunited.de>
2019-11-19 15:56:08 +0100

    add project script

--- index.php
+++ index.php
@@ -13,70 +13,27 @@
 
             <div class="grid">
                 <?php
-/*                echo "<div class='new-card radius-large'>
-                          <div class='card-overlay radius-large'>
-                              <div class='card-links'>
-                                  <a href='' target='_blank'>
-                                    <div class='button button-left'>More Info<i class='fas fa-info-circle'></i></div>
-                                  </a>
-                                  <a href='' target='_blank'>
-                                    <div class='button button-right'>Download<i class='fas fa-cloud-download-alt'></i></div>
-                                  </a>
+				$projects_file = file_get_contents(__DIR__ . '/assets/projects.json');
+				$projects = json_decode($projects_file, true);
+
+				foreach ($projects as $project) {
+					echo "<div class='new-card radius-large'>
+                              <div class='card-overlay radius-large'>
+                                  <div class='card-links'>
+                                      <a href='{$project['url_info']}' target='_blank'>
+                                        <div class='button button-left'>More Info<i class='fas fa-info-circle'></i></div>
+                                      </a>
+                                      <a href='{$project['url_dl']}' target='_blank'>
+                                        <div class='button button-right'>Download<i class='fas fa-cloud-download-alt'></i></div>
+                                      </a>
+                                  </div>
+                                  <div class='card-footer'>
+                                    <p>Last updated: {$project['last_updated']}</p>
+                                  </div>
                               </div>
-                              <div class='card-footer'>
-                                <p></p>
-                              </div>
-                          </div>
-                          <img class='card-img radius-large' src='' alt='cover'>
-                      </div>"; */?>
-                <div class='new-card radius-large'>
-                    <div class="card-overlay radius-large">
-                        <div class="card-links">
-                            <a href="https://atlas.nebulr.me" target="_blank">
-                                <div class="button button-left">More Info<i class="fas fa-info-circle"></i></div>
-                            </a>
-                            <a href="https://play.google.com/store/apps/details?id=me.nebulr.atlas" target="_blank">
-                                <div class="button button-right">Download<i class="fas fa-cloud-download-alt"></i></div>
-                            </a>
-                        </div>
-                        <div class="card-footer">
-                            <p>Last updated: 02/05/2019</p>
-                        </div>
-                    </div>
-                    <img class='card-img radius-large' src='assets/img/projects/atlas-cover.jpg' alt="cover">
-                </div>
-                <div class='new-card radius-large'>
-                    <div class="card-overlay radius-large">
-                        <div class="card-links">
-                            <a href="">
-                                <div class="button button-left">More Info<i class="fas fa-info-circle"></i></div>
-                            </a>
-                            <a href="https://github.com/xnebulr/SCS-for-Steam/releases" target="_blank">
-                                <div class="button button-right">Download<i class="fas fa-cloud-download-alt"></i></div>
-                            </a>
-                        </div>
-                        <div class="card-footer">
-                            <p>Last updated: 02/11/2016</p>
-                        </div>
-                    </div>
-                    <img class='card-img radius-large' src='assets/img/projects/scs-cover.jpg' alt="cover">
-                </div>
-                <div class='new-card radius-large'>
-                    <div class="card-overlay radius-large">
-                        <div class="card-links">
-                            <a href="">
-                                <div class="button button-left">More Info<i class="fas fa-info-circle"></i></div>
-                            </a>
-                            <a href="#">
-                                <div class="button-disabled button-right">Download<i class="fas fa-cloud-download-alt"></i></div>
-                            </a>
-                        </div>
-                        <div class="card-footer">
-                            <p>Last updated: 19/06/2014</p>
-                        </div>
-                    </div>
-                    <img class='card-img radius-large' src='assets/img/projects/st-cover.jpg' alt="cover">
-                </div>
+                              <img class='card-img radius-large' src='{$project['cover']}' alt='cover'>
+                          </div>";
+                } ?>
             </div>
         </main>
 
