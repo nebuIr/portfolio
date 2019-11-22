100f678da2009385fe0f2070b784b977608f9211
xnebulr <prime@nebulr.org>
2019-11-22 20:19:51 +0100

    add gtag, sitemap

--- assets/html/head.php
+++ assets/html/head.php
@@ -2,16 +2,20 @@
 $url = 'https://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
 ?>
 
+<!-- Basic -->
 <meta charset='UTF-8'>
 <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
-
 <link href='/assets/css/base.css' type='text/css' rel='stylesheet'/>
+
+<!-- External -->
 <link href='https://fonts.googleapis.com/css?family=Poppins:300,400,700,900&display=swap' rel='stylesheet'>
-
-<script src='https://apis.google.com/js/platform.js'></script>
 <script src='https://kit.fontawesome.com/8119fd0658.js' crossorigin='anonymous'></script>
-<script async defer src='https://buttons.github.io/buttons.js'></script>
 
+<!-- Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-68526906-5"></script>
+<script src="/assets/js/gtag.js"></script>
+
+<!-- Favicon -->
 <link rel="apple-touch-icon" sizes="180x180" href="/assets/img/favicon/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="/assets/img/favicon/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/assets/img/favicon/favicon-16x16.png">
