dc1a986ec52f27be5e5756ea114c18ca7ab3f316
prime@nebulr.org <prime@nebulr.org>
2019-12-20 10:37:57 +0100

    improve privacy policy layout

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 
 <body>
 
-<main class='main-text-medium'>
+<div class='main-text-medium'>
     <p class='title color-white weight-bold no-highlight'><img class="title-icon center-mobile" src="assets/img/logo/logo.svg" alt="logo">nebulr</p>
 
     <div class='grid'>
@@ -19,7 +19,7 @@
         $components->renderProjects('all');
         ?>
     </div>
-</main>
+</div>
 
 <?php include($_SERVER['DOCUMENT_ROOT'] . '/assets/html/footer.php'); ?>
 
