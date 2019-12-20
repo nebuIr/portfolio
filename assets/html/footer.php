1df533a598fab83ba0cc07080a4b53cff44124af
prime@nebulr.org <prime@nebulr.org>
2019-12-20 10:31:36 +0100

    improve footer

--- assets/html/footer.php
+++ assets/html/footer.php
@@ -1,7 +1,11 @@
 <footer class='radius-medium'>
     <p class='text-light'>This website was created by nebulr <img class="text-icon" src='/assets/img/logo/logo.svg' height='24px' width='24px'>
         <br>Feel free to <a href='mailto:support@nebulr.me'>send</a> any bug reports or feedback</p>
-    <a href='/privacypolicy'><div class="badge-small badge-white">Privacy Policy</div></a><br>
+    <p class="footer-title color-white weight-bold">About</p>
+    <a href='/privacypolicy' target='_blank'><div class='border-button'>
+            <i class='fas fa-user-shield'></i> Privacy Policy
+    </div></a><br>
+    <p class="footer-title color-white weight-bold">Social Links</p>
     <a href='https://github.com/xnebulr' target='_blank'><div class='border-button'>
         <i class='fab fa-github'></i> Github
     </div></a>
