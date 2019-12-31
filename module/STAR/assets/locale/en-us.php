de13c999b20f06c88ec941d6972138aa1313834a
Fabian <prime@nebulr.org>
2019-12-31 06:14:24 +0100

    fix STAR module locale

--- module/STAR/assets/locale/en-us.php
+++ module/STAR/assets/locale/en-us.php
@@ -16,6 +16,6 @@ $localeSCRR['REFERRAL_CODE_UPDATED'] = 'was updated!';
 $localeSCRR['REFERRAL_CODE_ADDED'] = 'was added!';
 $localeSCRR['REFERRAL_CODE_INVALID'] = 'is not valid, please check your formatting.';
 $localeSCRR['ENTER_REFERRAL_CODE'] = 'Please enter your referral code.';
-$localeSCRR['REFERRAL_CODE_ACTIVE'] = 'Once submitted, the code will remain active for 6 months.<br>Resubmitting the code will reset it's timer.';
+$localeSCRR['REFERRAL_CODE_ACTIVE'] = 'Once submitted, the code will remain active for 6 months.<br>Resubmitting the code will reset it\'s timer.';
 $localeSCRR['REFERRAL_CODE_REWARD'] = 'By using a referral code you\'ll get 5,000UEC for free!<br>That\'s an equivalent of $5.';
 $localeSCRR['CLIPBOARD'] = 'Copy to Clipboard';
