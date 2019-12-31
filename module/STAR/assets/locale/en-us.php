38378c405703fcae4c2bd36e1dcf152a8b39df4e
xnebulr <prime@nebulr.org>
2019-12-31 21:11:49 +0100

    update module STAR locale

--- module/STAR/assets/locale/en-us.php
+++ module/STAR/assets/locale/en-us.php
@@ -9,7 +9,7 @@ Language: English
 $localeSCRR = [];
 
 $localeSCRR['RANDOMIZER_TITLE'] = 'Star Citizen Referral Code Randomizer';
-$localeSCRR['RANDOMIZER_DESCRIPTION'] = 'Get a randomized referral code for Star Citizen and submit your own code!';
+$localeSCRR['RANDOMIZER_DESCRIPTION'] = 'Get a randomized referral code for Star Citizen and submit your own referral code!';
 $localeSCRR['RANDOMIZER_GET'] = 'Your randomized referral code for Star Citizen is:';
 $localeSCRR['RANDOMIZER_SET'] = 'Once you have your own referral code, submit it here:';
 $localeSCRR['THE_REFERRAL_CODE'] = 'The referral code';
@@ -17,6 +17,6 @@ $localeSCRR['REFERRAL_CODE_UPDATED'] = 'was updated!';
 $localeSCRR['REFERRAL_CODE_ADDED'] = 'was added!';
 $localeSCRR['REFERRAL_CODE_INVALID'] = 'is not valid, please check your formatting.';
 $localeSCRR['ENTER_REFERRAL_CODE'] = 'Please enter your referral code.';
-$localeSCRR['REFERRAL_CODE_ACTIVE'] = 'Once submitted, the code will remain active for 6 months.<br>Resubmitting the code will reset it\'s timer.';
+$localeSCRR['REFERRAL_CODE_ACTIVE'] = 'Once submitted, the referral code will remain active for 6 months.<br>Resubmitting the referral code will reset it\'s timer.';
 $localeSCRR['REFERRAL_CODE_REWARD'] = 'By using a referral code you\'ll get 5,000UEC for free!<br>That\'s an equivalent of $5.';
 $localeSCRR['CLIPBOARD'] = 'Copy to Clipboard';
