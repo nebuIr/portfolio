f3edf8e63b970a4c9083b9d8f355c52b94ce1238
Fabian <prime@nebulr.org>
2019-12-31 06:13:17 +0100

    update STAR module locale

--- module/STAR/assets/locale/de-de.php
+++ module/STAR/assets/locale/de-de.php
@@ -16,6 +16,6 @@ $localeSCRR['REFERRAL_CODE_UPDATED'] = 'wurde aktualisiert!';
 $localeSCRR['REFERRAL_CODE_ADDED'] = 'wurde hinzugefügt!';
 $localeSCRR['REFERRAL_CODE_INVALID'] = 'ist nicht gültig. Bitte überprüfe, ob die Formatierung korrekt ist.';
 $localeSCRR['ENTER_REFERRAL_CODE'] = 'Bitte gib deinen Empfehlungscode ein.';
-$localeSCRR['REFERRAL_CODE_ACTIVE'] = 'Einmal eingegeben, bleibt der Code für 6 Monate aktiv.<br>Die erneute Eingabe des Codes setzt den Timer zurück.';
+$localeSCRR['REFERRAL_CODE_ACTIVE'] = 'Einmal eingegeben, bleibt der Code für 6 Monate aktiv.<br>Die erneute Eingabe des Codes setzt dessen Timer zurück.';
 $localeSCRR['REFERRAL_CODE_REWARD'] = 'Durch die Verwendung eines Empfehlungscodes erhältst du 5.000UEC gratis!<br>Das entspricht einem Wert von $5.';
 $localeSCRR['CLIPBOARD'] = 'In die Zwischenablage kopieren';
