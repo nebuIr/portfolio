5ec59130b075a73bcac6a1fddf681ab446c03448
xnebulr <prime@nebulr.org>
2020-01-02 04:54:16 +0100

    update links

--- public/assets/html/footer.php
+++ public/assets/html/footer.php
@@ -4,17 +4,17 @@
     <p class='text-light'><?php echo $locale['CREDITS']?> <img class="text-icon" src='/../assets/img/logo/logo.svg' alt='logo' height='24px' width='24px'>
         <br><?php echo $locale['CONTACT_PREFIX'] ?> <a class='text-underline' href='mailto:<?php echo $locale['CONTACT_EMAIL'] ?>'><?php echo $locale['EMAIL'] ?></a> <?php echo $locale['CONTACT_SUFFIX'] ?></p>
     <p class='footer-title color-white weight-bold'><?php echo $locale['SOCIAL_LINKS'] ?></p>
-    <a href='https://github.com/xnebulr' target='_blank'><div class='border-button no-highlight'>
+    <a href='https://github.com/xnebulr' target='_blank' rel='nofollow'><div class='border-button no-highlight'>
             <i class='fab fa-github'></i> Github
         </div></a>
-    <a href='https://twitter.com/xnebulr' target='_blank'><div class='border-button no-highlight'>
+    <a href='https://twitter.com/xnebulr' target='_blank' rel='nofollow'><div class='border-button no-highlight'>
             <i class='fab fa-twitter'></i> Twitter
         </div></a>
-    <a href='https://youtube.com/nebulr' target='_blank'><div class='border-button no-highlight'>
+    <a href='https://youtube.com/nebulr' target='_blank' rel='nofollow'><div class='border-button no-highlight'>
             <i class='fab fa-youtube'></i> YouTube
         </div></a><br>
     <p class='footer-title color-white weight-bold'><?php echo $locale['ABOUT'] ?></p>
-    <a href='<?php echo $locale['LOCALE_SWITCH'] ?>'><div class='border-button no-highlight align-right flag <?php echo $locale['LOCALE_SWITCH_CLASS'] ?>'>
+    <a href='<?php echo $locale['LOCALE_SWITCH'] ?>' rel='canonical'><div class='border-button no-highlight align-right flag <?php echo $locale['LOCALE_SWITCH_CLASS'] ?>'>
             <i class='fas fa-language fa-lg hide show-on-hover'></i>
         </div></a>
     <a href='/../privacypolicy'><div class='border-button no-highlight'>
