d7f451c884e0389ad4074cb7416218f4de0e9d41
Fabian <prime@nebulr.org>
2020-08-23 01:23:07 +0200

    improve style.css, fix env

--- src/classes/Env.php
+++ src/classes/Env.php
@@ -17,9 +17,9 @@ class Env
         $dotenv_hash = Dotenv::createImmutable(__DIR__ . '/../../',  '.env.hash');
         $dotenv_hash->load();
 
-        $this->assetHashCss = getenv('ASSET_HASH_CSS');
-        $this->assetHashJs = getenv('ASSET_HASH_JS');
-        $this->assetHashRes = getenv('ASSET_HASH_RES');
+        $this->assetHashCss = $_ENV['ASSET_HASH_CSS'];
+        $this->assetHashJs = $_ENV['ASSET_HASH_JS'];
+        $this->assetHashRes = $_ENV['ASSET_HASH_RES'];
     }
 
     public function getAssetHashCss(): string
